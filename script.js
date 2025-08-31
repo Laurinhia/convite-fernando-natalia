6fa2138968bb2ec6c204cf7eb77f942d21090044
Laura <150156303+Laurinhia@users.noreply.github.com>
2025-08-30 21:10:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function abrirConvite() {
+  document.getElementById("envelope").style.display = "none";
+  document.getElementById("convite").classList.remove("hidden");
+}
+
+function confirmar() {
+  alert("Obrigado! Sua presença foi confirmada. 💍");
+}
